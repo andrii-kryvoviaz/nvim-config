21581146b935a6c706d6719ec68b98b57aac9503
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2024-05-25 13:33:30 +0300

    Update nvim-treesitter configuration

--- lua/default/plugins/treesitter.lua
+++ lua/default/plugins/treesitter.lua
@@ -9,54 +9,52 @@ return {
     local treesitter = require("nvim-treesitter.configs")
     -- configure treesitter
     treesitter.setup({
-    -- enable syntax highlighting
-    highlight = {
-      enable = true,
-    },
-    -- enable indentation
-    indent = { enable = true },
-    -- enable autotagging (w/ nvim-ts-autotag plugin)
-    autotag = { enable = true },
-    -- ensure these language parsers are installed
-    ensure_installed = {
-      "json",
-      "xml",
-      "yaml",
-      "proto",
-      "javascript",
-      "typescript",
-      "lua",
-      "php",
-      "go",
-      "vim",
-      "prisma",
-      "graphql",
-      "sql",
-      "html",
-      "css",
-      "tsx",
-      "svelte",
-      "angular",
-      "markdown",
-      "markdown_inline",
-      "bash",
-      "make",
-      "dockerfile",
-      "gitignore"
-    },
-    -- auto install above language parsers
-    auto_install = true,
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        keymaps = {
-          init_selection = "gnn", -- set to `false` to disable one of the mappings
-          node_incremental = "grn",
-          scope_incremental = "grc",
-          node_decremental = "grm",
-        }
+      -- enable syntax highlighting
+      highlight = {
+        enable = true,
       },
-    },
-  })
+      -- enable indentation
+      indent = { enable = true },
+      -- enable autotagging (w/ nvim-ts-autotag plugin)
+      autotag = { enable = true },
+      -- ensure these language parsers are installed
+      ensure_installed = {
+        "json",
+        "xml",
+        "yaml",
+        "proto",
+        "javascript",
+        "typescript",
+        "lua",
+        "php",
+        "go",
+        "vim",
+        "prisma",
+        "graphql",
+        "sql",
+        "html",
+        "css",
+        "tsx",
+        "svelte",
+        "angular",
+        "markdown",
+        "markdown_inline",
+        "bash",
+        "make",
+        "dockerfile",
+        "gitignore"
+      },
+      -- auto install above language parsers
+      auto_install = true,
+      	incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<CR>",
+          node_incremental = "<CR>",
+          scope_incremental = "<Tab>",
+          node_decremental = "<S-Tab>",
+        },
+      },
+    })
   end
 }
